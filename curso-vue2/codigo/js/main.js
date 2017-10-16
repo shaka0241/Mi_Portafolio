ca8531eb68b9a821ec78564054cfe87f7bbcafb1
Alberto Rojas <shaka0241@gmail.com>
2017-10-16 18:56:10 -0400

    Directivas de interacion

--- curso-vue2/codigo/js/main.js
+++ curso-vue2/codigo/js/main.js
@@ -3,6 +3,7 @@ new Vue ({
 	data: {
 		texto: 'Hola mundo desde VUE2',
 		nombre: 'Nombre por defecto',
-        nota:5        
+        nota:5,
+        peliculas: ['Batman vs Superman','La verdad duele', 'Los mercenarios', 'Spiderman 3']
 	}
 });

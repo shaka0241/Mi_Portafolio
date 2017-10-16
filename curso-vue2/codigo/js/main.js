0999a2e2fdead21886805af76554df676aff3d31
Alberto Rojas <shaka0241@gmail.com>
2017-10-16 19:08:57 -0400

    Mostrar listado colección de objetos

--- curso-vue2/codigo/js/main.js
+++ curso-vue2/codigo/js/main.js
@@ -4,6 +4,12 @@ new Vue ({
 		texto: 'Hola mundo desde VUE2',
 		nombre: 'Nombre por defecto',
         nota:5,
-        peliculas: ['Batman vs Superman','La verdad duele', 'Los mercenarios', 'Spiderman 3']
+        peliculas: ['Batman vs Superman','La verdad duele', 'Los mercenarios', 'Spiderman 3'],
+        frutas: [
+            {nombre: 'Manzana', temporada: 'Invierno', precio: 'Bajo'},
+            {nombre: 'Naranja', temporada: 'Otoño', precio: 'Medio'},
+            {nombre: 'Cereza', temporada: 'Primeravera', precio: 'Alto'},
+            {nombre: 'Sandia', temporada: 'Verano', precio: 'Medio'}
+        ]
 	}
 });

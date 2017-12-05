2dc2ed887b5cc0bd7f4e9b70eb66aa5e52fbab20
Alberto Rojas <shaka0241@gmail.com>
2017-12-05 14:55:32 -0400

    Agregado desarrollo de plataforma de Chat con NodeJS y SocketIO

---
+++ curso-sockets/client/main.js
@@ -0,0 +1,34 @@
+var socket = io.connect('http://162.122.177.16:6677',{'forceNew':true});
+
+socket.on('messages', function(data){
+	console.log(data);
+	render(data);
+});
+
+function render(data){
+	var html = data.map(function(message, index){
+
+		return(`
+			<div class="message">
+				<strong>${message.nickname}</strong> dice:
+					<p>${message.text}</p>
+				</div>	
+			`);
+
+	}).join(' ');
+
+	document.getElementById('messages').innerHTML = html;
+}
+
+function addMessage(e){
+	var message = {
+		nickname: document.getElementById('nickname').value,
+		text: document.getElementById('text').value
+	};
+
+	document.getElementById('nickname').style.diplay = 'none';
+	socket.emit('add-message', message);
+
+	return false;
+
+}
---
+++ curso-sockets/server/index.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+app.use(express.static('client'));
+
+app.get('/hola-mundo', function(req, res){
+	res.status(200).send('Hola mundo desde una ruta');
+});
+
+var messages = [{
+	id: 1,
+	text: 'Bienvenido al chat privado de Socket.io y NodeJS de Alberto Rojas..',
+	nickname: 'Bot - shaka0241'
+}];
+
+io.on('connection', function(socket){
+	console.log("El cliente con IP: "+socket.handshake.address+" se ha conectado...");
+
+	socket.emit('messages', messages);
+
+	socket.on('add-message', function(data){
+		messages.push(data);
+
+		io.sockets.emit('messages', messages);
+
+	});
+
+
+
+});
+
+server.listen(6677, function(){
+	console.log('Servidor esta funcionando en http://localhost:6677');
+});
